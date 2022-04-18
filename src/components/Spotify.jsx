d7d920aef4a87e3941545a25f1d1f6f5b501fd51
NahrulK <khayattullah73@gmail.com>
2022-04-18 18:04:59 +0800

    Spotify Component

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Body from "./Body";
 
 export default function Spotify() {
   return (
     <Container>
       <div className="spotify__body">
+        <Sidebar />
         <div className="body">
-          <div className="body__contents"></div>
+          <Navbar />
+          <div className="body__contents">
+            <Body />
+          </div>
         </div>
       </div>
-      <div className="spotify__footer"></div>
+      <div className="spotify__footer">
+        <Footer />
+      </div>
     </Container>
   );
 }
